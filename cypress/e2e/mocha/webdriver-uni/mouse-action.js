e6a4fde0e3919b70be018c34688b616cf4e9e84b
Amarendra91 <amarendra867@gmail.com>
2024-07-22 22:45:17 +0530

    drag and drop feature added

--- cypress/e2e/mocha/webdriver-uni/mouse-action.js
+++ cypress/e2e/mocha/webdriver-uni/mouse-action.js
@@ -5,7 +5,14 @@ describe('Verify mouse actions', () => {
     // cy.visit('/'); it will work if the baseurl mentioned
     cy.visit(Cypress.env('testurlone'));
   });
-  it('', () => {
-    cy.log('Hello world');
+  it('User should be able to perform drag and drop.', () => {
+    cy.get('#actions')
+      .scrollIntoView()
+      .invoke('removeAttr', 'target')
+      .click({ force: true });
+    cy.get('#draggable').trigger('mousedown', { which: 1 });
+    cy.get('#droppable')
+      .trigger('mousemove')
+      .trigger('mouseup', { force: true });
   });
 });

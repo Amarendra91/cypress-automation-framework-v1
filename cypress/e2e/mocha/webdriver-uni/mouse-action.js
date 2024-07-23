b7564691c65f24a494f8c783888e621fc535586f
Amarendra91 <amarendra867@gmail.com>
2024-07-23 11:12:57 +0530

    left mouse hold functionality

--- cypress/e2e/mocha/webdriver-uni/mouse-action.js
+++ cypress/e2e/mocha/webdriver-uni/mouse-action.js
@@ -15,11 +15,23 @@ describe('Verify mouse actions', () => {
       .trigger('mousemove')
       .trigger('mouseup', { force: true });
   });
-  it.only('User should be able to perform double mouse click.', () => {
+  it('User should be able to perform double mouse click.', () => {
     cy.get('#actions')
       .scrollIntoView()
       .invoke('removeAttr', 'target')
       .click({ force: true });
     cy.get('#double-click').dblclick();
+  });
+  it.only('User should be able to hold the left mouse click on a specific item', () => {
+    cy.get('#actions')
+      .scrollIntoView()
+      .invoke('removeAttr', 'target')
+      .click({ force: true });
+    cy.get('#click-box')
+      .trigger('mousedown', { which: 1 })
+      .then(($el) => {
+        expect($el).to.have.css('background-color', 'rgb(0, 255, 0)');
+        expect($el).to.contain('Well done!');
+      });
   });
 });
